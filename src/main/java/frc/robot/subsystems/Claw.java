6c941e4b97a4f2e36f5180ec5b93fef4e8c1bbca
Keith Kania <115370835+KeithsterTheGreat@users.noreply.github.com>
2023-01-14 11:55:56 -0800

    Created Claw Subsystem (#14)
    * Created Claw Subsystem
    
    * added claw subsystem into robot.java
    
    * initialized clamp and intake states in constructor
    
    * spotless
    
    * spotless
    
    * made subsystems private final in robot.java
    
    * spotless
    
    Co-authored-by: NolanKnight <nolan.r.knight@gmail.com>

---
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems;
+
+import frc.robot.shared.Subsystem;
+
+public class Claw implements Subsystem {
+  private ClampState m_clampState;
+  private IntakeState m_intakeState;
+
+  private enum ClampState {
+    Open,
+    Closed
+  }
+
+  private enum IntakeState {
+    In,
+    Out,
+    Neutral
+  }
+
+  public Claw() {
+    m_clampState = ClampState.Open;
+    m_intakeState = IntakeState.Neutral;
+  }
+
+  public void open() {
+    m_clampState = ClampState.Open;
+  }
+
+  public void close() {
+    m_clampState = ClampState.Closed;
+  }
+
+  public void intake() {
+    m_intakeState = IntakeState.In;
+  }
+
+  public void outtake() {
+    m_intakeState = IntakeState.Out;
+  }
+
+  public void stopIntake() {
+    m_intakeState = IntakeState.Neutral;
+  }
+
+  public void update() {
+    switch (m_clampState) {
+      case Open:
+        break;
+      case Closed:
+        break;
+      default:
+        break;
+    }
+    switch (m_intakeState) {
+      case In:
+        break;
+      case Out:
+        break;
+      case Neutral:
+        break;
+      default:
+        break;
+    }
+  }
+
+  public void reset() {
+    m_clampState = ClampState.Open;
+    m_intakeState = IntakeState.Neutral;
+  }
+}
