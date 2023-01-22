c4ac2bac3b5481683deee574da5b0928a0426f16
NolanKnight <91164617+NolanKnight@users.noreply.github.com>
2023-01-22 13:39:08 -0800

    Auto init (#22)
    * created auto framework
    
    * added auto command logic
    
    * spotless
    
    * fixed logic errors
    
    * Added javadocs
    
    * spotless
    
    * fixed logic errors
    
    * spotless
    
    * fixed logic errors
    
    * fixed more logic errors
    
    * deleted logic error
    
    * spotless
    
    * moved run command in sequential command
    
    * fixed run command loop structure
    
    * spotless
    
    * fixed logic error
    
    * finished logic
    
    * added elevator and wrist constants
    
    * Added init logic
    
    * fixed init problem for commands
    
    * deleted unnecessary code
    
    * spotless

---
+++ src/main/java/frc/robot/auto/commands/ExampleCommand.java
@@ -0,0 +1,20 @@
+package frc.robot.auto.commands;
+
+import frc.robot.shared.AutoCommand;
+
+public class ExampleCommand extends AutoCommand {
+  public void init() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public boolean isCompleted() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/auto/commands/util/WaitCommand.java
@@ -0,0 +1,26 @@
+package frc.robot.auto.commands.util;
+
+import frc.robot.shared.AutoCommand;
+
+public class WaitCommand extends AutoCommand {
+  private final double m_targetMsec;
+
+  /**
+   * The Constructor for the Wait Command class.
+   *
+   * @param targetMsec The target millisecond amount to wait.
+   */
+  public WaitCommand(double targetMsec) {
+    this.m_targetMsec = targetMsec;
+  }
+
+  public void init() {
+    setTargetMsec(m_targetMsec);
+  }
+
+  public void run() {}
+
+  public boolean isCompleted() {
+    return hasElapsed();
+  }
+}
