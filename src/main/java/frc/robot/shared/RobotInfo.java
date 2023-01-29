9f4762aaf4304aeb80c89a13b3960d135d116806
NolanKnight <nolan.r.knight@gmail.com>
2023-01-29 14:46:20 -0800

    updated can ids

--- src/main/java/frc/robot/shared/RobotInfo.java
+++ src/main/java/frc/robot/shared/RobotInfo.java
@@ -7,26 +7,26 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 public final class RobotInfo {
   public static final String CANIVORE_NAME = "Canivore";
 
-  public static final int COMPRESSOR_ID = 4;
+  public static final int COMPRESSOR_ID = 0;
 
   public static class IntakeInfo {
     public static final int FX_ID = 0;
   }
 
-  public static class WristInfo {
-    public static final double GEAR_RATIO = (1.0 / 3.0) * (1.0 / 3.0) * (22.0 / 44.0);
-  }
-
   public static class ArmInfo {
-    public static final int FX_ID = 0;
-    public static final int SOLENOID_ID = 1;
+    public static final int SOLENOID_ID = 0;
+    public static final int FX_ID = 1;
   }
 
   public static class ElevatorInfo {
-    public static final int FX_ID = 1;
-    public static final int FOLLOWER_FX_ID = 2;
-    public static final int BOTTOM_HALL_SENSOR_ID = 3;
-    public static final int TOP_HALL_SENSOR_ID = 4;
+    public static final int BOTTOM_HALL_SENSOR_ID = 0;
+    public static final int TOP_HALL_SENSOR_ID = 1;
+    public static final int FX_ID = 2;
+    public static final int FOLLOWER_FX_ID = 3;
+  }
+
+  public static class WristInfo {
+    public static final double GEAR_RATIO = (1.0 / 3.0) * (1.0 / 3.0) * (22.0 / 44.0);
   }
 
   public static class DriveInfo {
