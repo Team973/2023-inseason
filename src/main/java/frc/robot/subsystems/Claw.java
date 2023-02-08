a730464e2cfe5de28fb5f4529ebf9050bfd8ee31
Keith Kania <115370835+KeithsterTheGreat@users.noreply.github.com>
2023-02-07 20:26:46 -0800

    Add wrist control (#81)
    * added wrist motor and wrist control
    fixes #76
    
    * fix can ID
    
    * Tested Claw, .3 speed

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -6,6 +6,7 @@ import frc.robot.shared.RobotInfo;
 import frc.robot.shared.Subsystem;
 
 import com.ctre.phoenixpro.configs.TalonFXConfiguration;
+import com.ctre.phoenixpro.controls.PositionDutyCycle;
 import com.ctre.phoenixpro.hardware.TalonFX;
 import com.ctre.phoenixpro.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenixpro.signals.InvertedValue;
@@ -19,14 +20,16 @@ import lombok.experimental.Accessors;
 @Accessors(prefix = "m_")
 public class Claw implements Subsystem {
   @Setter @Getter private IntakeState m_intakeState;
-
   @Setter @Getter private GamePiece m_currentGamePiece;
+  @Setter @Getter private WristState m_wristState = WristState.Manual;
 
   private final TalonFX m_intakeMotor;
+  private final TalonFX m_wristMotor;
 
   private double m_targetAngle = 0.0;
   private double m_intakeStator = 0.0;
   private double m_intakeMotorOutput = 0.0;
+  @Setter private double m_wristMotorOutput = 0.0;
   private double m_statorCurrentLimit = 60.0;
 
   public enum GamePiece {
@@ -40,8 +43,19 @@ public class Claw implements Subsystem {
     Neutral
   }
 
+  public enum WristState {
+    Manual,
+    ClosedLoop
+  }
+
   public Claw() {
     m_intakeMotor = new TalonFX(ClawInfo.INTAKE_FX_ID, RobotInfo.CANIVORE_NAME);
+    m_wristMotor = new TalonFX(ClawInfo.WRIST_FX_ID, RobotInfo.CANIVORE_NAME);
+    configIntakeMotor();
+    configWristMotor();
+  }
+
+  private void configIntakeMotor() {
     var motorConfig = new TalonFXConfiguration();
 
     // Motor Directions
@@ -75,6 +89,39 @@ public class Claw implements Subsystem {
     m_intakeMotor.getConfigurator().apply(motorConfig);
   }
 
+  private void configWristMotor() {
+    var motorConfig = new TalonFXConfiguration();
+
+    // Motor Directions
+    motorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+    // Neutral Mode
+
+    motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    // Current limits
+
+    motorConfig.CurrentLimits.SupplyCurrentLimit = 40;
+    motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    motorConfig.CurrentLimits.StatorCurrentLimit = 80;
+    motorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+
+    // Motor feedback
+    motorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+    // Ramp rate
+    motorConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.0;
+    motorConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = 0.0;
+
+    // Velocity PID Parameters
+
+    motorConfig.Slot0.kP = 0.0;
+    motorConfig.Slot0.kI = 0.0;
+    motorConfig.Slot0.kD = 0.0;
+    motorConfig.Slot0.kS = 0.0;
+    m_wristMotor.getConfigurator().apply(motorConfig);
+  }
+
   public void setIntakeMotorOutput(double percent) {
     m_intakeMotorOutput = percent;
   }
@@ -84,7 +131,7 @@ public class Claw implements Subsystem {
     return Rotation2d.fromRotations(rot).getDegrees();
   }
 
-  public void setClawTargetAngle(double angle) {
+  public void setWristTargetAngle(double angle) {
     m_targetAngle = angle;
   }
 
@@ -100,6 +147,17 @@ public class Claw implements Subsystem {
     m_intakeStator = m_intakeMotor.getStatorCurrent().getValue();
 
     m_intakeMotor.set(m_intakeMotorOutput);
+
+    switch (m_wristState) {
+      case Manual:
+        m_wristMotor.set(m_wristMotorOutput);
+        break;
+      case ClosedLoop:
+        m_wristMotor.setControl(new PositionDutyCycle(m_targetAngle / 360.0 / ClawInfo.GEAR_RATIO));
+        break;
+      default:
+        break;
+    }
 
     SmartDashboard.putNumber("Intake Stator", m_intakeStator);
     SmartDashboard.putNumber("Intake Supply", m_intakeMotor.getSupplyCurrent().getValue());
