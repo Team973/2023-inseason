7d8e69c623f60c0a923f00362c849cc34306212a
NolanKnight <nolan.r.knight@gmail.com>
2023-02-11 16:15:52 -0800

    deleted trajectory code

--- src/main/java/frc/robot/auto/commands/TrajectoryManager.java
+++ src/main/java/frc/robot/auto/commands/TrajectoryManager.java
@@ -35,8 +35,9 @@ public class TrajectoryManager {
   private Trajectory createTrajectoryA() {
     Pose2d start = new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.fromDegrees(0.0));
     List<Translation2d> waypoints =
-        Arrays.asList(new Translation2d(3.35, 0.0), new Translation2d(4.57, 1.52));
-    Pose2d end = new Pose2d(new Translation2d(2.44, 1.52), Rotation2d.fromDegrees(0.0));
+        Arrays.asList(
+            new Translation2d(1.0, 1.0), new Translation2d(2.0, 0.0), new Translation2d(1.0, -1.0));
+    Pose2d end = new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.fromDegrees(0.0));
     return TrajectoryGenerator.generateTrajectory(start, waypoints, end, m_config);
   }
 
