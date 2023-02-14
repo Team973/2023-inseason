760b4226319aed193c4d2b6ccae318d2d1920f78
NolanKnight <nolan.r.knight@gmail.com>
2023-02-13 21:43:47 -0800

    implemented intake command

--- src/main/java/frc/robot/auto/commands/IntakeCommand.java
+++ src/main/java/frc/robot/auto/commands/IntakeCommand.java
@@ -5,15 +5,18 @@ import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Claw.GamePiece;
 import frc.robot.subsystems.Claw.IntakeState;
 
-public class ScoreGamePieceCommand extends AutoCommand {
+public class IntakeCommand extends AutoCommand {
   private final Claw m_claw;
+
+  private final IntakeState m_state;
 
   private final GamePiece m_gamePiece;
 
   private final double m_timeout;
 
-  public ScoreGamePieceCommand(Claw claw, GamePiece gamePiece, double timeout) {
+  public IntakeCommand(Claw claw, IntakeState state, GamePiece gamePiece, double preset, double timeout) {
     m_claw = claw;
+    m_state = state;
     m_gamePiece = gamePiece;
     m_timeout = timeout;
   }
@@ -23,11 +26,12 @@ public class ScoreGamePieceCommand extends AutoCommand {
     setTargetMsec(m_timeout);
     m_claw.setCurrentGamePiece(m_gamePiece);
 
-    m_claw.setIntakeState(IntakeState.Out);
+    m_claw.setIntakeState(m_state);
   }
 
   @Override
-  public void run() {}
+  public void run() {
+  }
 
   @Override
   public boolean isCompleted() {
