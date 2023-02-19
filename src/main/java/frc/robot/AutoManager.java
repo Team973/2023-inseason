5d3bb45dfae1a0e7da1309b402c4e7aa8d004490
NolanKnight <nolan.r.knight@gmail.com>
2023-02-19 14:38:44 -0800

    added timeout to intake in command

--- src/main/java/frc/robot/AutoManager.java
+++ src/main/java/frc/robot/AutoManager.java
@@ -46,30 +46,28 @@ public class AutoManager {
     m_drive = drive;
     m_trajectoryManager = trajectoryManager;
 
-    test =
-        new SequentialCommand(
-            new SetDrivePositionCommand(
-                drive,
-                new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(180)),
-                Rotation2d.fromDegrees(180.0)),
-            new DriveTrajectoryCommand(m_drive, m_trajectoryManager.getTrajectoryA()));
+    test = new SequentialCommand(
+        new SetDrivePositionCommand(
+            drive,
+            new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(180)),
+            Rotation2d.fromDegrees(180.0)),
+        new DriveTrajectoryCommand(m_drive, m_trajectoryManager.getTrajectoryA()));
 
-    oneCone =
-        new SequentialCommand(
-            new SetDrivePositionCommand(
-                drive,
-                new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(180)),
-                Rotation2d.fromDegrees(180.0)),
-            new IntakeCommand(claw, IntakeState.In, GamePiece.Cone, 0),
-            new ElevatorPresetCommand(elevator, Elevator.Presets.high, 4000),
-            new WaitCommand(500),
-            new WristAngleCommand(claw, Claw.ConePresets.high, 2000),
-            new IntakeCommand(claw, IntakeState.Out, GamePiece.Cone, 1500),
-            new WaitCommand(500),
-            new ConcurrentCommand(
-                new ElevatorPresetCommand(elevator, Elevator.Presets.stow, 1000),
-                new WristAngleCommand(claw, Claw.ConePresets.stow, 2000)),
-            new DriveTrajectoryCommand(m_drive, m_trajectoryManager.getTrajectoryA()));
+    oneCone = new SequentialCommand(
+        new SetDrivePositionCommand(
+            drive,
+            new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(180)),
+            Rotation2d.fromDegrees(180.0)),
+        new IntakeCommand(claw, IntakeState.In, GamePiece.Cone, 100),
+        new ElevatorPresetCommand(elevator, Elevator.Presets.high, 4000),
+        new WaitCommand(500),
+        new WristAngleCommand(claw, Claw.ConePresets.high, 2000),
+        new IntakeCommand(claw, IntakeState.Out, GamePiece.Cone, 1500),
+        new WaitCommand(500),
+        new ConcurrentCommand(
+            new ElevatorPresetCommand(elevator, Elevator.Presets.stow, 1000),
+            new WristAngleCommand(claw, Claw.ConePresets.stow, 2000)),
+        new DriveTrajectoryCommand(m_drive, m_trajectoryManager.getTrajectoryA()));
   }
 
   public void run() {
