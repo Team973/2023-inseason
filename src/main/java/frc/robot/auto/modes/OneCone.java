67d5cd23fa99905cf0227f2ef1fd6585bb62179c
Chris Lawson <Chris2fourlaw@users.noreply.github.com>
2023-02-26 13:32:35 -0800

    Cleanup claw presets (#134)

--- src/main/java/frc/robot/auto/modes/OneCone.java
+++ src/main/java/frc/robot/auto/modes/OneCone.java
@@ -6,13 +6,14 @@ import frc.robot.auto.commands.IntakeCommand;
 import frc.robot.auto.commands.SetCurrentGamePieceCommand;
 import frc.robot.auto.commands.SetDrivePositionCommand;
 import frc.robot.auto.commands.TrajectoryManager;
-import frc.robot.auto.commands.WristAngleCommand;
+import frc.robot.auto.commands.WristPresetCommand;
 import frc.robot.auto.commands.util.ConcurrentCommand;
 import frc.robot.auto.commands.util.SequentialCommand;
 import frc.robot.auto.commands.util.WaitCommand;
 import frc.robot.shared.Constants.GamePiece;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Claw.IntakeState;
+import frc.robot.subsystems.Claw.WristPreset;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Elevator;
 
@@ -27,12 +28,12 @@ public class OneCone extends SequentialCommand {
         new IntakeCommand(claw, IntakeState.In, 100),
         new ElevatorPresetCommand(elevator, Elevator.Presets.high, 4000),
         new WaitCommand(500),
-        new WristAngleCommand(claw, Claw.ConePresets.high, 2000),
+        new WristPresetCommand(claw, WristPreset.High, 2000),
         new IntakeCommand(claw, IntakeState.Out, 1500),
         new WaitCommand(500),
         new ConcurrentCommand(
             new ElevatorPresetCommand(elevator, Elevator.Presets.stow, 1000),
-            new WristAngleCommand(claw, Claw.ConePresets.stow, 2000)),
+            new WristPresetCommand(claw, WristPreset.Stow, 2000)),
         new DriveTrajectoryCommand(drive, trajectoryManager.getTrajectoryA()));
   }
 }
