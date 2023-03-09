1fc68a89839481685f493fe622e5e0a502f7a8e2
Oliver <owcurry42@gmail.com>
2023-03-08 19:56:25 -0800

    tuned holonomic controller

--- src/main/java/frc/robot/auto/modes/Test.java
+++ src/main/java/frc/robot/auto/modes/Test.java
@@ -1,14 +1,14 @@
 package frc.robot.auto.modes;
 
-import frc.robot.auto.commands.SetDrivePositionCommand;
+import frc.robot.auto.TrajectoryManager;
+import frc.robot.auto.commands.PathPlannerTrajectoryCommand;
 import frc.robot.auto.commands.util.SequentialCommand;
 import frc.robot.subsystems.Drive;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-
 public class Test extends SequentialCommand {
   public Test(Drive drive) {
-    super(new SetDrivePositionCommand(drive, new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(180))));
+    super(
+        new PathPlannerTrajectoryCommand(
+            drive, TrajectoryManager.getPath(TrajectoryManager.DriveBack)));
   }
 }
