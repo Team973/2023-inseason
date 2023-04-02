d1f488ddf192cac550167b94985164eac187471b
Chris Lawson <Chris2fourlaw@users.noreply.github.com>
2023-04-02 16:21:21 -0700

    Add profiling mode, remove high-utilizaiton brake mode setting (#181)
    * Add profiling mode, remove high-utilizaiton brake mode setting
    
    * fix version

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -433,11 +433,6 @@ public class Robot extends TimedRobot {
       SmartDashboard.putString("DB/String 1", m_selectedAutoSide.toString());
       SmartDashboard.putString("DB/String 2", m_preloadGamePiece.toString());
 
-      if (m_driverStick.getAButton()) {
-        m_drive.enableBrakeMode();
-      } else {
-        m_drive.disableBrakeMode();
-      }
     } catch (Exception e) {
       logException(e);
     }
